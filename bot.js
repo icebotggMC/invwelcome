289e7ee7be17565324f4c1ddea9e48b1b77c25f5
Dood <41833445+icebotggMC@users.noreply.github.com>
2019-03-17 20:26:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,15 +81,6 @@ client.on('guildMemberAdd', member => {
 
 
 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(`:rose:  ولكم في سيرفر Sky City:rose: 
-:crown:اسم العضو  ${member}:crown:  
-انت العضو رقم ${member.guild.memberCount} `) 
-}).catch(console.error)
-})
-
-
 
 
 
